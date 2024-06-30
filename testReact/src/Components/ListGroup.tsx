da7dcf2314379a3795b45d6354651a370e36d9b8
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-06-30 18:36:46 +0300

    Created a small list component

--- testReact/src/Components/ListGroup.tsx
+++ testReact/src/Components/ListGroup.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const getMessage = () => {
     return items.length === 0 && <p>No items found</p>;
   };
@@ -26,6 +27,7 @@ function ListGroup({ items, heading }: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
